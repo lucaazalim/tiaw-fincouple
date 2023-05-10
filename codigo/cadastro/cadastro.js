76049e6904e90f710c71e43575157d35685bcd02
Sidney <129969173+Sidromel@users.noreply.github.com>
2023-05-10 20:08:34 -0300

    Mudanças de cadastro e login

--- codigo/cadastro/cadastro.js
+++ codigo/cadastro/cadastro.js
@@ -27,128 +27,128 @@ let validsenha1 = false
 let Erro = document.querySelector('#Erro')
 let Certo = document.querySelector('#Certo')
 
-nome.addEventListener('keyup', ()=>{
-    if(nome.value.length <= 2){
+nome.addEventListener('keyup', () => {
+    if (nome.value.length <= 2) {
         labelnome.setAttribute('style', 'color: red')
         labelnome.innerHTML = 'Nome *mínimo de 3 caracteres'
         nome.setAttribute('style', 'border-color: red')
         validnome = false
-    }else{
+    } else {
         labelnome.setAttribute('style', 'color: green')
         labelnome.innerHTML = 'Nome'
         nome.setAttribute('style', 'border-color: green')
         validnome = true
     }
 })
-nome1.addEventListener('keyup', ()=>{
-    if(nome1.value.length <= 2){
+nome1.addEventListener('keyup', () => {
+    if (nome1.value.length <= 2) {
         labelnome1.setAttribute('style', 'color: red')
         labelnome1.innerHTML = 'Nome *mínimo de 3 caracteres'
         nome1.setAttribute('style', 'border-color: red')
         validnome1 = false
-    }else{
+    } else {
         labelnome1.setAttribute('style', 'color: green')
         labelnome1.innerHTML = 'Nome'
         nome1.setAttribute('style', 'border-color: green')
         validnome1 = true
     }
 })
-sobrenome.addEventListener('keyup', ()=>{
-    if(sobrenome.value.length <= 4){
+sobrenome.addEventListener('keyup', () => {
+    if (sobrenome.value.length <= 4) {
         labelsobrenome.setAttribute('style', 'color: red')
         labelsobrenome.innerHTML = 'Sobrenome *mínimo de 5 caracteres'
         sobrenome.setAttribute('style', 'border-color: red')
         validsobrenome = false
-    }else{
+    } else {
         labelsobrenome.setAttribute('style', 'color: green')
         labelsobrenome.innerHTML = 'Sobenome'
         sobrenome.setAttribute('style', 'border-color: green')
         validsobrenome = true
     }
 })
-sobrenome1.addEventListener('keyup', ()=>{
-    if(sobrenome1.value.length <= 4){
+sobrenome1.addEventListener('keyup', () => {
+    if (sobrenome1.value.length <= 4) {
         labelsobrenome1.setAttribute('style', 'color: red')
         labelsobrenome1.innerHTML = 'Sobrenome *mínimo de 5 caracteres'
         sobrenome1.setAttribute('style', 'border-color: red')
         validsobrenome1 = false
-    }else{
+    } else {
         labelsobrenome1.setAttribute('style', 'color: green')
         labelsobrenome1.innerHTML = 'Sobenome'
         sobrenome1.setAttribute('style', 'border-color: green')
         validsobrenome1 = true
     }
 })
-usuario.addEventListener('keyup', ()=>{
-    if(usuario.value.length <= 7){
+usuario.addEventListener('keyup', () => {
+    if (usuario.value.length <= 7) {
         labelusuario.setAttribute('style', 'color: red')
         labelusuario.innerHTML = 'Usuário *mínimo de 8 caracteres'
         usuario.setAttribute('style', 'border-color: red')
-        validusurario    = false
-    }else{
+        validusurario = false
+    } else {
         labelusuario.setAttribute('style', 'color: green')
         labelusuario.innerHTML = 'Usuário'
         usuario.setAttribute('style', 'border-color: green')
         validusurario = true
     }
 })
-usuario1.addEventListener('keyup', ()=>{
-    if(usuario1.value.length <= 7){
+usuario1.addEventListener('keyup', () => {
+    if (usuario1.value.length <= 7) {
         labelusuario1.setAttribute('style', 'color: red')
         labelusuario1.innerHTML = 'Usuário *mínimo de 8 caracteres'
         usuario1.setAttribute('style', 'border-color: red')
-        validusurario1    = false
-    }else{
+        validusurario1 = false
+    } else {
         labelusuario1.setAttribute('style', 'color: green')
         labelusuario1.innerHTML = 'Usuário'
         usuario1.setAttribute('style', 'border-color: green')
         validusurario1 = true
     }
 })
-senha.addEventListener('keyup', ()=>{
-    if(senha.value.length <= 5){
+senha.addEventListener('keyup', () => {
+    if (senha.value.length <= 5) {
         labelsenha.setAttribute('style', 'color: red')
         labelsenha.innerHTML = 'Senha *mínimo de 6 caracteres'
         senha.setAttribute('style', 'border-color: red')
         validsenha = false
-    }else{
+    } else {
         labelsenha.setAttribute('style', 'color: green')
         labelsenha.innerHTML = 'Senha'
         senha.setAttribute('style', 'border-color: green')
         validsenha = true
     }
 })
-senha1.addEventListener('keyup', ()=>{
-    if(senha1.value.length <= 5){
+senha1.addEventListener('keyup', () => {
+    if (senha1.value.length <= 5) {
         labelsenha1.setAttribute('style', 'color: red')
         labelsenha1.innerHTML = 'Senha *mínimo de 6 caracteres'
         senha1.setAttribute('style', 'border-color: red')
         validsenha1 = false
-    }else{
+    } else {
         labelsenha1.setAttribute('style', 'color: green')
         labelsenha1.innerHTML = 'Senha'
         senha1.setAttribute('style', 'border-color: green')
         validsenha1 = true
     }
 })
-btn.addEventListener('click', ()=>{
+btn.addEventListener('click', () => {
     let inputSenha = document.querySelector('#senha')
-    if(inputSenha.getAttribute('type') == 'password'){
+    if (inputSenha.getAttribute('type') == 'password') {
         inputSenha.setAttribute('type', 'text')
-    }else{
+    } else {
         inputSenha.setAttribute('type', 'password')
     }
 })
-btn1.addEventListener('click', ()=>{
+btn1.addEventListener('click', () => {
     let inputSenha1 = document.querySelector('#senha1')
-    if(inputSenha1.getAttribute('type') == 'password'){
+    if (inputSenha1.getAttribute('type') == 'password') {
         inputSenha1.setAttribute('type', 'text')
-    }else{
+    } else {
         inputSenha1.setAttribute('type', 'password')
     }
 })
-function cadastrar(){
-    if(validnome && validsenha && validsobrenome && validusurario && validnome1 && validsenha1 && validsobrenome1 && validusurario1){
+function cadastrar() {
+    if (validnome && validsenha && validsobrenome && validusurario && validnome1 && validsenha1 && validsobrenome1 && validusurario1) {
         let listaUser = JSON.parse(localStorage.getItem('listaUser') || '[]')
         listaUser.push(
             {
@@ -168,13 +168,13 @@ function cadastrar(){
         Certo.innerHTML = '<strong>Cadastrando usuário... .</strong>'
         Erro.setAttribute('style', 'display: none')
         Erro.innerHTML = ''
-        setTimeout(()=>{
-            window.location.href='../login'
-        },3000)
-} else {
-    Erro.setAttribute('style', 'display: block')
-    Erro.innerHTML = '<strong>Preencha todos os campos!</strong>'
-    Certo.setAttribute('style', 'display: none')
-    Certo.innerHTML = ''
-}
+        setTimeout(() => {
+            window.location.href = '../login'
+        }, 3000)
+    } else {
+        Erro.setAttribute('style', 'display: block')
+        Erro.innerHTML = '<strong>Preencha todos os campos!</strong>'
+        Certo.setAttribute('style', 'display: none')
+        Certo.innerHTML = ''
+    }
 }
--- codigo/login/login.js
+++ codigo/login/login.js
@@ -1,21 +1,21 @@
 let btn = document.querySelector('.fa-eye')
-btn.addEventListener('click', ()=>{
+btn.addEventListener('click', () => {
     let inputSenha = document.querySelector('#senha')
-    if(inputSenha.getAttribute('type') == 'password'){
+    if (inputSenha.getAttribute('type') == 'password') {
         inputSenha.setAttribute('type', 'text')
-    }else{
+    } else {
         inputSenha.setAttribute('type', 'password')
     }
 })
 
-function entrar(){
+function entrar() {
     let usuario = document.querySelector('#usuario')
     let usuariolabel = document.querySelector('#usuariolabel')
     let senha = document.querySelector('#senha')
     let senhalabel = document.querySelector('#senhalabel')
     let Erro = document.querySelector('#Erro')
     let listaUser = []
-    let userValid = {
+    var userValid = {
         nome: '',
         sobrenome: '',
         usuario: '',
@@ -26,8 +26,8 @@ function entrar(){
         senha1: ''
     }
     listaUser = JSON.parse(localStorage.getItem('listaUser'))
-    listaUser.forEach((item) =>{
-        if(usuario.value == item.usuario && senha.value == item.senha && usuario1.value == item.usuario1 && senha1.value == item.senha1){
+    listaUser.forEach((item) => { 
+        if ((usuario.value == item.usuario) && (senha.value == item.senha) || ((usuario.value == item.usuario1) && (senha.value == item.senha1))) {
             userValid = {
                 nome: item.nome,
                 sobrenome: item.sobrenome,
@@ -38,13 +38,17 @@ function entrar(){
                 usuario1: item.usuario1,
                 senha1: item.senha1
             }
+            var usuariovalido = JSON.stringify(userValid)
+            var usuariologado = localStorage.setItem('user', usuariovalido)
+            setTimeout(() => {
+                window.location.href = '../area-logada'
+            }, 1000)
         }
     })
-    if(usuario.value == userValid.usuario && senha.value == userValid.senha || usuario1.value == userValid.usuario1 && senha1.value == userValid.senha1){
-        window.location.href='../area-logada'
+    if ((usuario.value == userValid.usuario) && (senha.value == userValid.senha) || ((usuario.value == userValid.usuario1) && (senha.value == userValid.senha1))) {
         let token = Math.random().toString(16).substring(2)
         localStorage.setItem('token', token)
-    }else{
+    } else {
         usuariolabel.setAttribute('style', 'color: red')
         usuario.setAttribute('style', 'border-color: red')
         senhalabel.setAttribute('style', 'color: red')
@@ -53,5 +57,4 @@ function entrar(){
         Erro.innerHTML = 'Usuário ou senha incorretos!'
         usuario.focus()
     }
-
 }
